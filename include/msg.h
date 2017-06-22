51bb77cef7642a41c71a6ac5bc282e661f54e8bf
Han Sangwoo <goldhightree@gmail.com>
2017-06-22 18:21:24 +0900

    Change TYPE number

--- include/msg.h
+++ include/msg.h
@@ -7,16 +7,18 @@
 #define TYPE_SERVER 1
 #define TYPE_CLIENT 2
 
-#define TYPE_REQ_PUT 1
-#define TYPE_REQ_GET 2
-#define TYPE_REQ_REMOVE 3
-#define TYPE_RES_GET 4
-#define TYPE_QUIT 5
+#define TYPE_REQ_PUT 10
+#define TYPE_REQ_GET 20
+#define TYPE_REQ_REMOVE 30
+#define TYPE_RES_GET 40
+#define TYPE_QUIT 50
 
 #define NODATA -1
 
 typedef struct msgbuf {
+    /* message queue type */
     long mtype;
+    /* custom type */
     long type;
     unsigned int key;
     char value[VALUESIZE];
